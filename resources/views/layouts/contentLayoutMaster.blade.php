37799b15134a0e28647a1975099255decd274f0c
SOSTheBlack <jeancesargarcia@gmail.com>
2023-04-15 00:31:27 -0300

    chre: criados os menus

--- resources/views/layouts/contentLayoutMaster.blade.php
+++ resources/views/layouts/contentLayoutMaster.blade.php
@@ -3,12 +3,12 @@
 {!! Helper::updatePageConfig($pageConfigs) !!}
 @endisset
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 @php
-// confiData variable layoutClasses array in Helper.php file.
-$configData = Helper::applClasses();
+    // confiData variable layoutClasses array in Helper.php file.
+    $configData = Helper::applClasses();
 @endphp
-<!--
+    <!--
 Template Name: Materialize - Material Design Admin Template
 Author: PixInvent
 Website: http://www.pixinvent.com/
@@ -21,22 +21,22 @@ License: You must have a valid license purchased only from themeforest(the above
 
 -->
 <html class="loading"
-  lang="@if(session()->has('locale')){{session()->get('locale')}}@else{{$configData['defaultLanguage']}}@endif"
-  data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}">
+      lang="@if(session()->has('locale')){{session()->get('locale')}}@else{{$configData['defaultLanguage']}}@endif"
+      data-textdirection="{{ env('MIX_CONTENT_DIRECTION') === 'rtl' ? 'rtl' : 'ltr' }}">
 <!-- BEGIN: Head-->
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title') | Materialize - Material Design Admin Template</title>
-  <link rel="apple-touch-icon" href="../../images/favicon/apple-touch-icon-152x152.png">
-  <link rel="shortcut icon" type="image/x-icon" href="../../images/favicon/favicon-32x32.png">
-
-  {{-- Include core + vendor Styles --}}
-  @include('panels.styles')
+    <title>@yield('title') | Materialize - Material Design Admin Template</title>
+    <link rel="apple-touch-icon" href="../../images/favicon/apple-touch-icon-152x152.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../images/favicon/favicon-32x32.png">
+    <script src="https://kit.fontawesome.com/846a7a1872.js" crossorigin="anonymous"></script>
+    {{-- Include core + vendor Styles --}}
+    @include('panels.styles')
 
 </head>
 <!-- END: Head-->
@@ -44,11 +44,11 @@ License: You must have a valid license purchased only from themeforest(the above
 {{-- @isset(config('custom.custom.mainLayoutType'))
 @endisset --}}
 @if(!empty($configData['mainLayoutType']) && isset($configData['mainLayoutType']))
-@include(($configData['mainLayoutType'] === 'horizontal-menu') ? 'layouts.horizontalLayoutMaster':
-'layouts.verticalLayoutMaster')
+    @include(($configData['mainLayoutType'] === 'horizontal-menu') ? 'layouts.horizontalLayoutMaster':
+    'layouts.verticalLayoutMaster')
 @else
-{{-- if mainLaoutType is empty or not set then its print below line  --}}
-<h1>{{'mainLayoutType Option is empty in config custom.php file.'}}</h1>
+    {{-- if mainLaoutType is empty or not set then its print below line  --}}
+    <h1>{{'mainLayoutType Option is empty in config custom.php file.'}}</h1>
 @endif
 
 </html>
